45a587fe0758036883c6ce1704f93f5019329a3f
blusk1n <tahaznazen@gmail.com>
2019-11-19 09:45:43 +0100

    recursion review

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(name) {
+	var array = [] ;
+	var content = document.body.children
+	if(document.body.classList.contains(name)){
+					array.push(document.body)
+				}
+	function look(nodes){
+		for(var i = 0 ; i<nodes.length;i++){
+				if(nodes[i].classList.contains(name)){
+					array.push(nodes[i])
+				}
+				if(nodes[i].children !== undefined){
+					look(nodes[i].children)
+				}
+		}	
+	}	
+	look(content)
+	console.log(array + "1")
+		return array
+
+}
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+	 var res = [];
+  	if(typeof obj === "string") {
+  		return  '"' + obj + '"';
+  	}
+  	else if(typeof obj === "boolean" || typeof obj === "number" || obj === null){
+  		return String(obj)
+  	}
+  	else if(obj === undefined || typeof obj==="function"){
+  		return "null"
+  	}
+  	 
+
+
+
+  	 if(Array.isArray(obj) === true){
+  		obj.forEach(function(element,i){
+  			res.push(stringifyJSON(element))
+  		})	
+  		 return '[' + res + ']'	
+  		}
+
+  	if(typeof obj ==="object"){
+  		for (var key in obj){
+  			if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue ;
+      	}
+  			 res.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
+  		}
+  		return '{' + res + '}'
+  	}
+
+
+
+  	
+  	  
+}  ;
